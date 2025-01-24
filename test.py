65269d1907a31c479915c254145a50c2d4cec94c
celestial-bard <zhaoge999@hotmail.com>
2025-01-24 20:32:46 +0800

    1st upload

---
+++ test.py
@@ -0,0 +1,8 @@
+from typing import Dict, Union
+from exp_methods.forecast_mae_api import ForecastMAE
+# from exp_methods.demo_interface import DeMo
+# from exp_methods.qcnet_interface import QCNet
+
+model = ForecastMAE()
+# model = QCNet()
+# model = DeMo()
